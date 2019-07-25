e9bdd19fadb39e0d4db3604faa90793c7396a27d
mladuke <mladuke@mladuke.com>
2019-07-24 22:28:37 -0400

    largest number

---
+++ Greedy/Largest_number.py
@@ -0,0 +1,11 @@
+def Largest(list):
+    list = sorted(list)
+    numS =""
+    for i in range(len(list)):
+        numS = numS + str(list.pop())
+    return int(numS)
+
+list = [1,3,2,5,6,4]
+print(Largest(list))
+
+# Returns 654321
