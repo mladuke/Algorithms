4170cd87c06fd3aab23b10f0322e07ceeddf43d3
mladuke <mladuke@mladuke.com>
2019-07-27 22:55:40 -0400

    Added Output as comments

--- Greedy/minGroups.py
+++ Greedy/minGroups.py
@@ -29,3 +29,21 @@ print("Input List: ",L)
 print("Range allowed in a group: ", r)
 print("Number of Groups formed: ", count)
 print("List of Groups: ", R)
+print()
+L =[3.6,3.8,4.2,4.6,5,5.5,6,7,8]
+r  = 5
+R , count = pointsCoveredSorted(L,r)
+print("Input List: ",L)
+print("Range allowed in a group: ", r)
+print("Number of Groups formed: ", count)
+print("List of Groups: ", R)
+
+# Input List:  [3.6, 3.8, 4.2, 4.6, 5, 5.5, 6, 7, 8]
+# Range allowed in a group:  1
+# Number of Groups formed:  3
+# List of Groups:  [(3.6, 4.6), (5, 6), (7, 8)]
+#
+# Input List:  [3.6, 3.8, 4.2, 4.6, 5, 5.5, 6, 7, 8]
+# Range allowed in a group:  5
+# Number of Groups formed:  1
+# List of Groups:  [(3.6, 8.6)]
