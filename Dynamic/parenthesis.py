9ea5741aacdf95c52c752de833c897a4ddaddf2d
mladuke <mladuke@mladuke.com>
2019-08-11 23:41:58 -0400

    Updated formatting

--- Dynamic/parenthesis.py
+++ Dynamic/parenthesis.py
@@ -1,8 +1,5 @@
-# Uses python3
 from math import inf
-
 import numpy as np
-
 
 def evalt(a, b, op):
     if op == '+':
@@ -14,7 +11,6 @@ def evalt(a, b, op):
     else:
         assert False
 
-
 def min_and_max(m, M, op, i, j):
     tmp_min = inf
     tmp_max = -inf
@@ -24,12 +20,9 @@ def min_and_max(m, M, op, i, j):
         b = evalt(m[i][k], m[k + 1][j], op[k])
         c = evalt(M[i][k], M[k + 1][j], op[k])
         d = evalt(m[i][k], M[k + 1][j], op[k])
-
         tmp_min = min(tmp_min, a, b, c, d)
         tmp_max = max(tmp_max, a, b, c, d)
-
     return (tmp_min, tmp_max)
-
 
 def get_maximum_value(dataset):
     op = dataset[1:len(dataset):2]
@@ -40,13 +33,10 @@ def get_maximum_value(dataset):
     for i in range(n):
         m[i][i] = int(d[i])
         M[i][i] = int(d[i])
-
     for s in range(1, n):
         for i in range(n - s):
             j = i + s
             m[i][j], M[i][j] = min_and_max(m, M, op, i, j)
     return M[0][n - 1]
 
-
-if __name__ == "__main__":
-    print(get_maximum_value(input()))
+print(get_maximum_value("5-8+7*4-8+9"))
